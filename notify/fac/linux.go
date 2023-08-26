ed474fef5740dcfd933128adf051589b9317f632
ogios <2134692955@qq.com>
2023-08-26 22:52:11 +0800

    feat: add notify-send for linux

--- notify/fac/linux.go
+++ notify/fac/linux.go
@@ -3,11 +3,33 @@
 
 package fac
 
-import "gosocket/app"
+import (
+	"gosocket/app"
+	"os/exec"
+)
 
 type Linux struct{}
 
 func (n *Linux) Notify(item app.Notification) error {
+	params := []string{
+		"--app-name=",
+		item.AppID,
+		item.Title,
+		item.Content,
+	}
+	if len(item.IconPath) > 0 {
+		params = append(
+			[]string{
+				"--icon=",
+				item.IconPath,
+			},
+			params...,
+		)
+	}
+	cmd := exec.Command("notify-send", params...)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
 	return nil
 }
 
